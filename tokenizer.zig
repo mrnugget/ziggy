0619722e34ec1bbe38db115fa497a61c8d12b17e
Thorsten Ball <mrnugget@gmail.com>
2023-10-14 19:27:38 +0200

    More code for the tokenizer

--- tokenizer.zig
+++ tokenizer.zig
@@ -9,6 +9,35 @@ test "zig std tokenizer" {
     try std.testing.expectEqual(tok.next().tag, .eof);
 }
 
+test "zig std tokenizer list" {
+    const std = @import("std");
+
+    // Setup allocator
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    // Setup TokenList
+    var tokens = std.zig.Ast.TokenList{};
+    defer tokens.deinit(allocator);
+    try tokens.ensureTotalCapacity(allocator, 100);
+
+    // Setup tokenizer
+    const source = "fn addNums(a: i32, b: i32) i32 { }";
+    var tokenizer = std.zig.Tokenizer.init(source);
+
+    // Get all the items into a single list
+    while (true) {
+        const token = tokenizer.next();
+        try tokens.append(allocator, .{
+            .tag = token.tag,
+            .start = @as(u32, @intCast(token.loc.start)),
+        });
+        if (token.tag == .eof) break;
+    }
+
+    try std.testing.expectEqual(@as(usize, 15), tokens.len);
+}
+
 test "zig std parser" {
     const std = @import("std");
     const expectEqual = std.testing.expectEqual;
